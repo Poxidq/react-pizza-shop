ac195aef6b13f7937bfceb9af657f6c82d11bc1f
Snyf <43717805+Poxidq@users.noreply.github.com>
2020-10-25 14:32:36 +0300

    вынесли header, как компонент; создали компоненты Button, Categories и SortPopup

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classNames from "classnames";
+
+const Button = ({ onClick, outline, children, className }) => {
+  return (
+    <button
+      className={classNames("button", className, { "button-outline": outline })}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
